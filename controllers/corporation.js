9e82e571eebc8a6a6da03e84e3aaba8c88ebc187
cvweiss <cvweiss@gmail.com>
2019-08-23 01:31:58 +0000

    removed limit, removed debug corp

--- controllers/corporation.js
+++ controllers/corporation.js
@@ -14,7 +14,7 @@ async function getData(req, res) {
   if (details.length == 0) return undefined;
 
   o.details = details[0];
-  o.characters = await req.app.mysql.query('select distinct ew.character_id id, name, date_format(start_date, "%Y/%m/%d %H:%i") start_date from ew_characters ew left join ew_history eh on ew.character_id = eh.character_id where ew.corporation_id = 98409330 and eh.corporation_id = 98409330 and end_date is null order by start_date desc limit 50', req.params.id, req.params.id);
+  o.characters = await req.app.mysql.query('select distinct ew.character_id id, name, date_format(start_date, "%Y/%m/%d %H:%i") start_date from ew_characters ew left join ew_history eh on ew.character_id = eh.character_id where ew.corporation_id = ? and eh.corporation_id = ? and end_date is null order by start_date desc', [req.params.id, req.params.id]);
 
   if (o.details.memberCount > 0 && o.details.ceoID > 1) {
     const ceo = await req.app.mysql.query('select character_id, name from ew_characters where character_id = ?', o.details.ceoID);
