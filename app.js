8afcf4b66fc3506f290fbbf08a6c2ee103749f49
cvweiss <cvweiss@gmail.com>
2019-08-25 22:58:00 +0000

    removed stack trace from logs

--- app.js
+++ app.js
@@ -67,7 +67,8 @@ app.use(function(err, req, res, next) {
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-	next(createError(404));
+    res.sendStatus(404);
+    next();
 });
 
 module.exports = app;
