c844053e0adde4ae9342a35b20c9b5bfbbcc03c9
cvweiss <cvweiss@gmail.com>
2019-08-25 22:32:40 +0000

    added cors to api

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 });
 
 app.disable('x-powered-by');
-//app.use(require('cors')());
+app.use('/api/', require('cors')());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
