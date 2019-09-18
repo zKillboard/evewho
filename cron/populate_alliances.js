1ac6294c777bb54551817b57426f539fb1036e3e
cvweiss <cvweiss@gmail.com>
2019-09-18 01:33:13 +0000

    removed self call

--- cron/populate_alliances.js
+++ cron/populate_alliances.js
@@ -1,14 +1,9 @@
 module.exports = f;
 
 async function f(app) {
-    try{
-        let result = await app.phin('https://esi.evetech.net/latest/alliances/');
-        let alliances = result.body;
-        for (let i = 0; i < alliances.length; i++ ) {
-            await app.mysql.query('insert ignore into ew_alliances (alliance_id) values (?)', alliances[i]);
-        }
-        setTimeout(function() { f(app); }, 3600000);
-    } catch (e) {
-        console.log(e);
-    }
+  let result = await app.phin('https://esi.evetech.net/latest/alliances/');
+  let alliances = result.body;
+  for (let i = 0; i < alliances.length; i++ ) {
+    await app.mysql.query('insert ignore into ew_alliances (alliance_id) values (?)', alliances[i]);
+  }
 }
