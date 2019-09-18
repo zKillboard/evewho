b29dbf2f754d18f5bab385496526a433db3feb8e
cvweiss <cvweiss@gmail.com>
2019-09-18 03:29:01 +0000

    improvements

--- cron/update_corporations.js
+++ cron/update_corporations.js
@@ -9,17 +9,19 @@ async function f(app) {
         let corp_id = row.corporation_id;
 
         let url = 'https://esi.evetech.net/v4/corporations/' + corp_id + '/';
-        promises.push(app.phin(url).then(res => { parse(app, res, corp_id, url); }));
+        promises.push(app.phin(url).then(res => { parse(app, res, corp_id, url); }).catch(e => { failed(e, corp_id); }));
 
-        await app.sleep(100 + (app.error_count * 1000)); // Limit to 10/s + time for errors
+        let sleep = 100 + (app.error_count * 1000);
+        await app.sleep(sleep); // Limit to 10/s + time for errors
     }
-
     await Promise.all(promises).catch();
 }
 
 async function parse(app, res, corp_id, url) {
+  try {
     if (res.statusCode == 200) {
-        var body = res.body;
+        var body = JSON.parse(res.body);
+
         let r = await app.mysql.query('update ew_corporations set alliance_id = ?, faction_id = ?, ceoID = ?, memberCount = ?, name = ?, ticker = ?, taxRate = ? where corporation_id = ?', [d0(body.alliance_id), d0(body.faction_id), d0(body.ceo_id), d0(body.memberCount), body.name, body.ticker, d0(body.tax_rate), corp_id]);
         if (r.changedRows > 0) {
             await app.mysql.query('update ew_corporations set recalc = 1, lastUpdated = now() where corporation_id = ?', [corp_id]);
@@ -28,9 +30,16 @@ async function parse(app, res, corp_id, url) {
         }
     } else {
         app.error_count++;
-        console.log(res.statusCode + ' ' + url);
+        if (res.statusCode != 502) console.log(res.statusCode + ' ' + url);
         setTimeout(function() { app.error_count--; }, 1000);
     }
+  } catch (e) { 
+    console.log(url + ' ' + e);
+  }
+}
+
+async function failed(e, corp_id) {
+    console.log(e);
 }
 
 function d0(field) {
