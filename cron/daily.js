38a412ef769e016b2c292cb4b1096823f7c0d93c
cvweiss <cvweiss@gmail.com>
2019-09-20 00:34:37 +0000

    more daily queries

--- cron/daily.js
+++ cron/daily.js
@@ -6,4 +6,9 @@ async function daily(app) {
     await mysql.query("update ew_alliances set corp_count = 0 where memberCount = 0");
     await mysql.query("update ew_corporations set recalc = 1");
     await mysql.query("update ew_alliances set recalc = 1");
+
+    await mysql.query("update ew_alliances set mc_7 = mc_6, mc_6 = mc_5, mc_5 = mc_4, mc_4 = mc_3, mc_3 = mc_2, mc_2 = memberCount");
+    await mysql.query("update ew_corporations set mc_7 = mc_6, mc_6 = mc_5, mc_5 = mc_4, mc_4 = mc_3, mc_3 = mc_2, mc_2 = memberCount");
+    await mysql.query("update ew_corporations set diff = -1 * (mc_7 - memberCount)");
+    await mysql.query("update ew_alliances set diff = -1 * (mc_7 - memberCount)");
 }
