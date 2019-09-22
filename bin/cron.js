f5673c6f76d52df34abf196cc64222a4a55931d4
cvweiss <cvweiss@gmail.com>
2019-09-22 18:37:17 +0000

    recalculate corporations

--- bin/cron.js
+++ bin/cron.js
@@ -34,6 +34,7 @@ let tasks = {
     '../cron/update_corporations.js': { span: 15 },
     '../cron/update_alliances.js': { span: 15 },
     '../cron/recalculate_alliances.js': { span: 60 },
+    '../cron/recalculate_corporations.js': { span: 60 },
 }
 
 // Clear existing runnign keys
