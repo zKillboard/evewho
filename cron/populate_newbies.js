0840a4c35b2611988798cefd0bd6251cb9c19542
cvweiss <cvweiss@gmail.com>
2019-10-01 03:00:15 +0000

    only query when starting

--- cron/populate_newbies.js
+++ cron/populate_newbies.js
@@ -2,9 +2,13 @@ module.exports = f;
 
 const entity = require('../classes/entity.js');
 
+let max_char_id = undefined;
+
 async function f(app) {
     try {
-        let max_char_id = await app.mysql.queryField('char_id', 'select max(character_id) char_id from ew_characters where character_id > 2112000000 and character_id < 2200000000');
+        if (max_char_id === undefined) {
+            max_char_id = await app.mysql.queryField('char_id', 'select max(character_id) char_id from ew_characters where character_id > 2112000000 and character_id < 2200000000');
+        }
         let delta = Math.floor(Math.random() * 10) + 1;
         let next_id = max_char_id + delta;
 
@@ -13,9 +17,9 @@ async function f(app) {
             for (let i = max_char_id + 1; i <= next_id; i++) {
                 await entity.add(app, 'char', i);
             }
+            max_char_id = next_id;
         }
     } catch (e) {
         console.log(e);
-        // Can be ignored
     }
 }
