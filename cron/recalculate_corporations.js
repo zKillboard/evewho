4c96adcaefbd9897e450ec6ce1be9eaf352b2a07
cvweiss <cvweiss@gmail.com>
2019-10-05 16:38:13 +0000

    no need to calc avg sec status of npc corps

--- cron/recalculate_corporations.js
+++ cron/recalculate_corporations.js
@@ -6,12 +6,13 @@ async function f(app) {
         let row = result[i];
 
         let active, info;
-        if (row.corporation_id < 1999999) {
+        if (parseInt(row.corporation_id) < 1999999) {
             active = 9;
+            info = await app.mysql.queryRow('select count(1) count, 0 avg_sec_status from ew_characters where corporation_id =?', [row.corporation_id]);
         } else {
             active = await app.mysql.queryField('count', 'select count(1) count from ew_history where start_date >= date_sub(now(), interval 3 month) and corporation_id = ?', [row.corporation_id]);
-        }
             info = await app.mysql.queryRow('select count(1) count, avg(sec_status) avg_sec_status from ew_characters where corporation_id =?', [row.corporation_id]);
+        }
         let activity_level = Math.max(0, Math.min(9, Math.floor(Math.log(active))));
    
         await app.mysql.query('update ew_corporations set memberCount = ?, avg_sec_status = ?, active = ?, diff = -1 * (mc_7 - ?), recalc = 0 where corporation_id = ?', [info.count, info.avg_sec_status, activity_level, info.count, row.corporation_id]);
