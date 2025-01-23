03b671fb5e5baef90609edb014dd2712d432dde0
Mina1316 <mina1316.canreply@gmail.com>
2025-01-24 08:21:33 +0900

    feat: add Corporation Departed/Joined retrieve API

--- www/controllers/api.js
+++ www/controllers/api.js
@@ -10,6 +10,12 @@ async function getData(req, res) {
   if (req.params.type == 'character') return getChar(req, res);
   if (req.params.type == 'corplist') return getCorp(req, res);
   if (req.params.type == 'allilist') return getAlli(req, res);
+  
+  // CorpDeparted, change path if needed, this is draft!
+  if (req.params.type == 'corpdeparted') return getCorpDeparted(req, res);
+  // CorpJoined, change path if needed, this is draft!
+  if (req.params.type == 'corpjoined') return getCorpJoined(req, res);
+
   return {json: {}};
 }
 
@@ -47,3 +53,36 @@ async function getAlli(req, res) {
   return { json: {info: info, characters: characters}};
 }
 
+async function getCorpDeparted(req, res) {
+  const app = req.app.app;
+  
+  // TODO: Need to check compatibility, since Nullish coalscing needs node 14 at least.
+  const page = (req.query.page ?? 0)
+  const offset = 50 * (page - 1)
+  const info = await app.mysql.query('select corporation_id, name, memberCount from ew_corporations where is_npc_corp = 0 and corporation_id > 0 and corporation_id = ? limit 1', req.params.id);
+  
+  if (info.length == 0)return {status_code: 404} // 404;
+
+  query = 'select h.character_id id, name, date_format(start_date, "%Y/%m/%d %H:%i") start_date, date_format(end_date, "%Y/%m/%d %H:%i") end_date from ew_history h left join ew_characters c on h.character_id = c.character_id where end_date is not null and h.corporation_id = ? order by end_date desc limit 50 offset ?' 
+
+  const result = await app.mysql.query(query, [req.params.id, offset]) 
+  
+  return { json: {info: info, page: page, characters: result} } 
+}
+
+async function getCorpJoined(req, res) {
+  const app = req.app.app;
+  
+  // TODO: Need to check compatibility, since Nullish coalscing needs node 14 at least.
+  const page = (req.query.page ?? 0)
+  const offset = 50 * (page - 1)
+  const info = await app.mysql.query('select corporation_id, name, memberCount from ew_corporations where is_npc_corp = 0 and corporation_id > 0 and corporation_id = ? limit 1', req.params.id);
+  
+  if (info.length == 0)return {status_code: 404} // 404;
+
+  query = 'select h.character_id id, name, date_format(start_date, "%Y/%m/%d %H:%i") start_date, date_format(end_date, "%Y/%m/%d %H:%i") end_date from ew_history h left join ew_characters c on h.character_id = c.character_id where h.corporation_id = ? order by start_date desc limit 50 offset ?'
+  
+  const result = await app.mysql.query(query, [req.params.id, offset]) 
+  
+  return { json: {info: info, page: page, characters: result} } 
+}
