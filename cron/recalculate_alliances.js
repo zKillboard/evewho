1da881b66b10b9a441be6f799ed2125b3a1e93e0
cvweiss <cvweiss@gmail.com>
2025-05-24 16:25:11 +0000

    minor improvement here

--- cron/recalculate_alliances.js
+++ cron/recalculate_alliances.js
@@ -4,16 +4,12 @@ module.exports = {
 }
 
 async function f(app) {
-    let allis = undefined;
-    do {
-        allis = await app.mysql.query('select * from ew_alliances where recalc = 1 limit 1000');
-        for (let i = 0; i < allis.length; i++) {
-            let alli = allis[i];
-            //console.log('Recalculating alli ' + alli.alliance_id);
+    let allis = await app.mysql.query('select * from ew_alliances where recalc = 1 limit 1000');
+    for (let i = 0; i < allis.length; i++) {
+        let alli = allis[i];
 
-            let row = await app.mysql.queryRow('select count(*) count, avg(sec_status) avg_sec_status, count(distinct corporation_id) corp_count from ew_characters where alliance_id = ?', [alli.alliance_id]);
+        let row = await app.mysql.queryRow('select count(*) count, avg(sec_status) avg_sec_status, count(distinct corporation_id) corp_count from ew_characters where alliance_id = ?', [alli.alliance_id]);
 
-            await app.mysql.query('update ew_alliances set corp_count = ?, memberCount = ?, avg_sec_status = ?, diff = -1 * (mc_7 - ?), recalc = 0 where alliance_id = ?', [row.corp_count, row.count, row.avg_sec_status, row.count, alli.alliance_id]);
-        }
-    } while (allis.length > 0);
+        await app.mysql.query('update ew_alliances set corp_count = ?, memberCount = ?, avg_sec_status = ?, diff = -1 * (mc_7 - ?), recalc = 0 where alliance_id = ?', [row.corp_count, row.count, row.avg_sec_status, row.count, alli.alliance_id]);
+    }
 }
