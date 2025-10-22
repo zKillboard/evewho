63cbc6d4b88f07118fbfb3345ddc3c1edbe55d56
cvweiss <cvweiss@gmail.com>
2025-10-22 11:22:05 +0000

    bringing github up to date

--- cron/update_characters_full.js
+++ cron/update_characters_full.js
@@ -10,17 +10,22 @@ async function f(app) {
 
     let second = Math.round(Date.now() / 1000);
 
-    let chars = await app.mysql.query('select character_id, name, corporation_id, recent_change from ew_characters where recent_change = 1 limit 1');
-    if (chars.length == 0) chars = await app.mysql.query('select character_id, name, corporation_id, recent_change from ew_characters order by lastUpdated limit 100');
+    let recents = await app.mysql.query('select character_id, name, corporation_id, recent_change from ew_characters where recent_change = 1 limit 100')
+    let more =  await app.mysql.query('select character_id, name, corporation_id, recent_change from ew_characters order by lastUpdated limit 100');
+    let chars = [...recents, ...more];
 
+	let awaits = [];
     for (let i = 0; i < chars.length; i++) {
+        let r = await app.mysql.query('update ew_characters set lastUpdated = now() where character_id = ?', chars[i].character_id);
         if (chars[i].corporation_id == 1000001) {
-            let r = await app.mysql.query('update ew_characters set lastUpdated = now() where character_id = ?', chars[i].character_id);
             continue;
         }
         let row = chars[i];
         let url = 'https://esi.evetech.net/v5/characters/' + row.character_id + '/';
-        await app.phin(url).then(res => { characters.parse(app, res, row.character_id, url); }).catch(e => { console.log('error in phin', e); });
-        break;
-    }
+        console.log(url);
+		awaits.push(app.phin(url).then(res => { characters.parse(app, res, row.character_id, url); }).catch(e => { console.log('error in phin', e); }));
+        if (awaits.length >= 10) break;
+	}
+	await Promise.all(awaits);
 }
+
