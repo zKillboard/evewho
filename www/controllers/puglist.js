35ad757bded632ec45745175f913075db1de9155
cvweiss <cvweiss@gmail.com>
2025-11-01 20:46:03 -0400

    return proper data even for bad requests

--- www/controllers/puglist.js
+++ www/controllers/puglist.js
@@ -7,8 +7,8 @@ async function getData(req, res) {
 	const app = req.app.app;
 
 	let offset = 250 * (req.params.page - 1);
-	if (offset < 0 || offset > 10000) return null;
-	if (req.params.id < 1999999) return null; // Ignore NPC corps
+	if (offset < 0 || offset > 10000) return { package: { characters: [], left: [], right: [] }, ttl: 86400}; // 204 No Content
+	if (req.params.id < 1999999) return { package: { characters: [], left: [], right: [] }, ttl: 86400}; // Ignore NPC corps
 
 	let query, left = false, right = false;
 	switch (req.params.which) {
