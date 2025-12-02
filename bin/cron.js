a67cdbcf27d23af19ea8078bfdb6ff97189def3d
cvweiss <cvweiss@gmail.com>
2025-12-02 10:44:19 -0500

    override"

--- bin/cron.js
+++ bin/cron.js
@@ -1,1 +1,8 @@
-require('fundamen')('cron', {cron: process.argv[2]});
+const app = require('fundamen')('cron', {cron: process.argv[2]});
+
+// Override Fundamen's MySQL with our own Database class
+if (process.env.MYSQL_LOAD == 'true') {
+    const Database = require('../classes/Database.js');
+    app.mysql = new Database(process.env.MYSQL_URL);
+    console.log('Overrode MySQL with local Database class...');
+}
--- bin/www.js
+++ bin/www.js
@@ -1,1 +1,8 @@
 const app = require('fundamen')('www');
+
+// Override Fundamen's MySQL with our own Database class
+if (process.env.MYSQL_LOAD == 'true') {
+    const Database = require('../classes/Database.js');
+    app.mysql = new Database(process.env.MYSQL_URL);
+    console.log('Overrode MySQL with local Database class...');
+}
