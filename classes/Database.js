536c3b5372e5987ff0e29ddc3bb17a2761f192e0
cvweiss <cvweiss@gmail.com>
2025-12-02 10:40:25 -0500

    promised

--- classes/Database.js
+++ classes/Database.js
@@ -1,4 +1,4 @@
-const mysql = require( 'mysql2' );
+const mysql = require( 'mysql2/promise' );
 const url = require( 'url' );
 
 class Database {
@@ -17,14 +17,9 @@ class Database {
 		//this.connection = mysql.createConnection( config );
         this.connection  = mysql.createPool( config );
 	}
-	query( sql, args ) {
-		return new Promise( ( resolve, reject ) => {
-			this.connection.query( sql, args, ( err, rows ) => {
-				if ( err )
-					return reject( err );
-				resolve( rows );
-			} );
-		} );
+	async query( sql, args ) {
+		const [rows] = await this.connection.query( sql, args );
+		return rows;
 	}
     async queryRow(sql, args) {
         let result = await this.query(sql, args);
@@ -40,14 +35,8 @@ class Database {
         }
         return row;
     }
-	close() {
-		return new Promise( ( resolve, reject ) => {
-			this.connection.end( err => {
-				if ( err )
-					return reject( err );
-				resolve();
-			} );
-		} );
+	async close() {
+		await this.connection.end();
 	}
 }
 
