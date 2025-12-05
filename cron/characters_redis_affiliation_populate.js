df6f9f2e7e61507a65d4e6e74b3e65f6d88e10ae
cvweiss <cvweiss@gmail.com>
2025-12-05 14:38:07 -0500

    removal of forced index

--- cron/characters_redis_affiliation_populate.js
+++ cron/characters_redis_affiliation_populate.js
@@ -9,7 +9,7 @@ var m = false;
 var date = undefined;
 
 const affLong = 'select character_id, lastAffUpdated from ew_characters where lastAffUpdated <= date_sub(now(), interval 1 day) order by lastAffUpdated limit 1000';
-const affRecent = 'select character_id from ew_characters force index(lastAffUpdated_2)  where lastEmploymentChange >= date_sub(now(), interval 5 year) order by lastAffUpdated limit 250000';
+const affRecent = 'select character_id from ew_characters  where lastEmploymentChange >= date_sub(now(), interval 5 year) order by lastAffUpdated limit 250000';
  
 const bucketLong = 'evewho:affiliates';
 const bucketRecent = 'evewho:affiliates:recent';
