75dced1a361ab62cd0ebce9caa434d6e0285759e
cvweiss <cvweiss@gmail.com>
2026-05-29 19:44:51 -0400

    execute job in background

--- cron/update_characters_history.js
+++ cron/update_characters_history.js
@@ -8,21 +8,30 @@ const { HEADERS } = require('../classes/constants.js');
 const todaysDayOfMonth = new Date().getDate();
 
 async function f(app) {
+	runInBackground(app);
+}
+
+async function runInBackground(app) {
 	if (app.util.isDowntime()) return;
 
-	let promises = [];
-
 	try {
-		let chars = await app.mysql.query('select character_id, name, corporation_id from ew_characters where name != "" and history_added = 0 order by lastEmploymentChange desc limit 3');
+		let chars = await app.mysql.query('select character_id, name, corporation_id from ew_characters where name != "" and history_added = 0 limit 1');
 		for (let i = 0; i < chars.length; i++) {
 			if (app.pause420 == true) break;
 			if (app.error_count > 0) break;
 
 			let row = chars[i];
 
-			let corpurl = 'https://esi.evetech.net/characters/' + row.character_id + '/corporationhistory';
-			const res = await fetch(corpurl, HEADERS);
-			characters.parse_corps(app, res, row, corpurl).catch(e => console.log(e));
+			try {
+				await app.mysql.query('update ew_characters set history_added = 1 where character_id = ?', [row.character_id]);
+
+				let corpurl = 'https://esi.evetech.net/characters/' + row.character_id + '/corporationhistory';
+				const res = await fetch(corpurl, HEADERS);
+				await characters.parse_corps(app, res, row, corpurl);
+			} catch (e) {
+				await app.mysql.query('update ew_characters set history_added = 0 where character_id = ?', [row.character_id]);
+				console.error(e);
+			}
 		}
 	} catch (e) {
 		console.log(e);
