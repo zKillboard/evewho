b290f34e97956b4b9dfa9fdcc92de1c6c0f692f1
cvweiss <cvweiss@gmail.com>
2026-05-29 07:29:16 -0400

    try/catch

--- cron/update_characters_history.js
+++ cron/update_characters_history.js
@@ -1,6 +1,6 @@
 module.exports = {
-    exec: f,
-    span: 1
+	exec: f,
+	span: 1
 }
 
 const characters = require('../classes/characters.js');
@@ -10,18 +10,22 @@ const todaysDayOfMonth = new Date().getDate();
 async function f(app) {
 	if (app.util.isDowntime()) return;
 
-    let promises = [];
+	let promises = [];
 
-	let chars = await app.mysql.query('select character_id, name, corporation_id from ew_characters where name != "" and history_added = 0 order by lastEmploymentChange desc limit 3');
-    for (let i = 0; i < chars.length; i++ ){
-        if (app.pause420 == true) break;
-        if (app.error_count > 0) break;
+	try {
+		let chars = await app.mysql.query('select character_id, name, corporation_id from ew_characters where name != "" and history_added = 0 order by lastEmploymentChange desc limit 3');
+		for (let i = 0; i < chars.length; i++) {
+			if (app.pause420 == true) break;
+			if (app.error_count > 0) break;
 
-        let row = chars[i];
+			let row = chars[i];
 
-		let corpurl = 'https://esi.evetech.net/characters/' + row.character_id + '/corporationhistory';
-		const res = await fetch(corpurl, HEADERS);
-		promises.push(characters.parse_corps(app, res, row, corpurl));
-    }
-    await Promise.all(promises).catch();
+			let corpurl = 'https://esi.evetech.net/characters/' + row.character_id + '/corporationhistory';
+			const res = await fetch(corpurl, HEADERS);
+			promises.push(characters.parse_corps(app, res, row, corpurl));
+		}
+		await Promise.all(promises).catch();
+	} catch (e) {
+		console.log(e);
+	}
 }
