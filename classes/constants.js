7947b04987947aa5664bf5b84f20c3c755e701fb
cvweiss <cvweiss@gmail.com>
2026-05-29 19:35:43 -0400

    use a timeout, new fundamen

--- classes/constants.js
+++ classes/constants.js
@@ -1,5 +1,24 @@
-export const HEADERS = {
+const REQUEST_TIMEOUT_MS = 10_000;
+
+function createTimeoutSignal() {
+	if (typeof AbortSignal !== 'undefined' && typeof AbortSignal.timeout === 'function') {
+		return AbortSignal.timeout(REQUEST_TIMEOUT_MS);
+	}
+
+	return undefined;
+}
+
+const HEADERS = {
 	headers: {
 		'User-Agent': process.env.USER_AGENT
+	},
+	get signal() {
+		// Return a new timeout signal for each request.
+		return createTimeoutSignal();
 	}
+};
+
+module.exports = {
+	HEADERS,
+	REQUEST_TIMEOUT_MS
 };
