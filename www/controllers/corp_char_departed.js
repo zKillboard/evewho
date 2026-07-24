9a39a1d6979dfd53ba4abccfed6560db2f5d10b6
theqwert125 <55003315+theqwert125@users.noreply.github.com>
2026-07-24 12:10:27 -0400

    Extend recycled/closed display to corp/alliance departed list

--- www/controllers/corp_char_departed.js
+++ www/controllers/corp_char_departed.js
@@ -10,7 +10,7 @@ async function getData(req, res) {
 	if (offset < 0 || offset > 10000) return { package: { characters: [], entity_type: 'character' }, ttl: 86400 };
 	if (req.params.id < 1999999) return { package: { characters: [], entity_type: 'character' }, ttl: 86400 }; // Ignore NPC corps
 
-	const query = 'select h.character_id id, name, date_format(start_date, "%Y/%m/%d %H:%i") start_date, date_format(end_date, "%Y/%m/%d %H:%i") end_date from ew_history h left join ew_characters c on h.character_id = c.character_id where end_date is not null and h.corporation_id = ? order by end_date desc limit 250 offset ?';
+	const query = 'select h.character_id id, name, c.corporation_id, date_format(start_date, "%Y/%m/%d %H:%i") start_date, date_format(end_date, "%Y/%m/%d %H:%i") end_date from ew_history h left join ew_characters c on h.character_id = c.character_id where end_date is not null and h.corporation_id = ? order by end_date desc limit 250 offset ?';
 	let result = await app.mysql.query(query, [req.params.id, offset]);
 
 	return {
