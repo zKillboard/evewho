471b044915296901f00ec571c25989832ad2fcd2
cvweiss <cvweiss@gmail.com>
2026-09-11 08:02:42 -0400

    tweak to updating history

--- cron/update_characters_history.js
+++ cron/update_characters_history.js
@@ -8,32 +8,31 @@ const { HEADERS } = require('../classes/constants.js');
 const todaysDayOfMonth = new Date().getDate();
 
 async function f(app) {
-	runInBackground(app);
-}
-
-async function runInBackground(app) {
 	if (app.util.isDowntime()) return;
 
+	let chars = await app.mysql.query('select character_id, name, corporation_id from ew_characters where name != "" and history_added = 0 limit 5');
+	const checks = [];
+	for (let i = 0; i < chars.length; i++) {
+		if (app.pause420 == true) break;
+		if (app.error_count > 0) break;
+
+		checks.push(updateCharacterHistory(app, chars[i]));
+	}
+
+	const results = await Promise.allSettled(checks);
+	const failed = results.find(result => result.status == 'rejected');
+	if (failed) throw failed.reason;
+}
+
+async function updateCharacterHistory(app, row) {
 	try {
-		let chars = await app.mysql.query('select character_id, name, corporation_id from ew_characters where name != "" and history_added = 0 limit 1');
-		for (let i = 0; i < chars.length; i++) {
-			if (app.pause420 == true) break;
-			if (app.error_count > 0) break;
+		await app.mysql.query('update ew_characters set history_added = 1 where character_id = ?', [row.character_id]);
 
-			let row = chars[i];
-
-			try {
-				await app.mysql.query('update ew_characters set history_added = 1 where character_id = ?', [row.character_id]);
-
-				let corpurl = 'https://esi.evetech.net/characters/' + row.character_id + '/corporationhistory';
-				const res = await fetch(corpurl, HEADERS);
-				await characters.parse_corps(app, res, row, corpurl);
-			} catch (e) {
-				await app.mysql.query('update ew_characters set history_added = 0 where character_id = ?', [row.character_id]);
-				console.error(e);
-			}
-		}
+		let corpurl = 'https://esi.evetech.net/characters/' + row.character_id + '/corporationhistory';
+		const res = await fetch(corpurl, HEADERS);
+		await characters.parse_corps(app, res, row, corpurl);
 	} catch (e) {
-		console.log(e);
+		await app.mysql.query('update ew_characters set history_added = 0 where character_id = ?', [row.character_id]);
+		throw e;
 	}
 }
